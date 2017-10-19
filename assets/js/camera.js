80a6e10d21238655461ad23acde912eaa03fcb26
= <=>
2017-10-19 11:13:22 -0700

    modify headers

--- assets/js/camera.js
+++ assets/js/camera.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     var file = event.target.files[0];
 
     $.ajax({
-      url: 'https://api.einstein.ai/v2/vision/predict',
+      url: 'http://api.einstein.ai/v2/vision/predict',
       // beforeSend: function(request) {
       //   request.setRequestHeader('Authority', authorizationToken);
       // },
@@ -27,7 +27,8 @@ $(document).ready(function() {
           'HVBKD5M6YCW74BSJTUDHJTUXQBTM34HVUXCHJBBJ3WUIXXW4HVJQIZOVX7FQZ5GPYEZYPMUCY7EJHLW43F2AXB2UJ2KSQOA6DSUDSFQ',
         'Cache-Control': 'no-cache',
         'Content-Type': 'multipart/form-data',
-        'Access-Control-Allow-Credentials': true
+        'Access-Control-Allow-Credentials': '*',
+        'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS'
       }
       // 'Access-Control-Allow-Methods': 'POST',
       // 'Access-Control-Allow-Headers':
