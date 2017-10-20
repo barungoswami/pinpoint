ee2637d8f09ac5e5c43c7da31cf0d32e4c3612b4
= <=>
2017-10-20 16:05:57 -0700

    filReader

--- assets/js/camera.js
+++ assets/js/camera.js
@@ -2,38 +2,44 @@
 // You can't upload a filename from Javascript, the file has to be selected by the user from an <input type="file">
 
 $(document).ready(function() {
-  loadImage();
+  uploadImage();
 });
 
-var loadImage = function() {
+var uploadImage = function() {
   var uploadBtn = $('#upload-button');
   uploadBtn.change(function(event) {
-    event.preventDefault();
-    var file = event.target.files[0];
-    debugger;
     var formData = new FormData();
-    formData['sampleContent'] = file;
-    // formData['modelId'] = 'GeneralImageClassifier';
+    var file = event.target.files[0];
+    // debugger;
 
+    if ( window.FileReader ) {
+      reader = new FileReader();
+      reader.readAsDataURL(file);
+    }
+    if (formData) {
+      formData.append('sampleContent', file);
+      formData.append('modelId', 'GeneralImageClassifier');
+    }
+
+// requestApiServer
     $.ajax({
       url: 'https://api.einstein.ai/v2/vision/predict',
       method: 'POST',
       headers: {
         'Authorization':
           'Bearer X6VERA4KVSPIWMLJ7TXIYBSIIOZNL36E7MZQW7NFADXQ6ZBE6YLWHTGCOR6EPJP43MPC5GJW2CBOAP6IGINTC25NSVYEFFEHCTDUQZY',
-        'Cache-Control': 'no-cache',
-        'Content-Type': 'multipart/form-data'
+        'Cache-Control': 'no-cache'
+        // 'Content-Type': 'multipart/form-data'
       },
-      data: {
-        'modelId': 'GeneralImageClassifier',
-        // 'sampleContent': file
-      },
-      formData: formData,
+      data: formData,
+      // formData: formData,
+      // cache: false,
       processData: false,
       contentType: false
     }).done(function(res) {
       // format res in card format to show image and data
       console.log(res);
+      var probabilities = res.probabilities;
     });
   });
 };
