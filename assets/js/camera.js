1e3d9baf7e61bf07c630942b25f1885b3c272671
= <=>
2017-10-20 15:00:27 -0700

    latest push

--- assets/js/camera.js
+++ assets/js/camera.js
@@ -1,47 +1,39 @@
+
+// You can't upload a filename from Javascript, the file has to be selected by the user from an <input type="file">
+
 $(document).ready(function() {
+  loadImage();
+});
+
+var loadImage = function() {
   var uploadBtn = $('#upload-button');
-
-  // uploadBtn.click(function() {
-  //   // debugger;
-  // });
-
   uploadBtn.change(function(event) {
     event.preventDefault();
-    var file = event.target.files[0].name;
-
-    //  $.ajax({
-    // url: 'handler.ashx', // put a handler instead of direct path
-    // data: new FormData().append('file',
-    //       document.getElementById("image").files[0]),
-    // type: 'POST',
-    // success: function (data) {
-    //     $('#result').html('Image uploaded was: ' + data);
-    // }
-    // });
-
-    // var formData = {
-    //   sampleContent: file,
-    //   modelId: 'GeneralImageClassifier'
-    // };
+    var file = event.target.files[0];
+    debugger;
+    var formData = new FormData();
+    formData['sampleContent'] = file;
+    // formData['modelId'] = 'GeneralImageClassifier';
 
     $.ajax({
       url: 'https://api.einstein.ai/v2/vision/predict',
       method: 'POST',
-      data: {
-        sampleContent: file,
-        modelId: 'GeneralImageClassifier'
-      },
-      processData: false,
-      contentType: false,
       headers: {
-        Authorization:
-          'Bearer 2ADD2POY2IYHK7JJ3HVA4PF5VOHF3D3S2TG7AH5TJO6UNI476WD762OTWVJYU4QYUB24ZS6LGHFELJRCFMT7KIG2PPPQBNDI646UETQ',
-        'Cache-Control': 'no-cache'
+        'Authorization':
+          'Bearer X6VERA4KVSPIWMLJ7TXIYBSIIOZNL36E7MZQW7NFADXQ6ZBE6YLWHTGCOR6EPJP43MPC5GJW2CBOAP6IGINTC25NSVYEFFEHCTDUQZY',
+        'Cache-Control': 'no-cache',
         'Content-Type': 'multipart/form-data'
-      }
+      },
+      data: {
+        'modelId': 'GeneralImageClassifier',
+        // 'sampleContent': file
+      },
+      formData: formData,
+      processData: false,
+      contentType: false
     }).done(function(res) {
       // format res in card format to show image and data
       console.log(res);
     });
   });
-});
+};
