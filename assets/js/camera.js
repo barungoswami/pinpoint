3bc3b9fb152a3dd9b3626d47cb9c3ca631c23bbb
= <=>
2017-10-19 18:33:57 -0700

    data fields

--- assets/js/camera.js
+++ assets/js/camera.js
@@ -7,33 +7,41 @@ $(document).ready(function() {
 
   uploadBtn.change(function(event) {
     event.preventDefault();
-    debugger;
-    var file = event.target.files[0];
-    // console.log(file);
-    //
-    // var fd = new FormData();
-    // fd.append('file', input.files[0]);
+    var file = event.target.files[0].name;
+
+    //  $.ajax({
+    // url: 'handler.ashx', // put a handler instead of direct path
+    // data: new FormData().append('file',
+    //       document.getElementById("image").files[0]),
+    // type: 'POST',
+    // success: function (data) {
+    //     $('#result').html('Image uploaded was: ' + data);
+    // }
+    // });
+
+    // var formData = {
+    //   sampleContent: file,
+    //   modelId: 'GeneralImageClassifier'
+    // };
 
     $.ajax({
       url: 'https://api.einstein.ai/v2/vision/predict',
       method: 'POST',
-      // data:
-      // processData: false,
-      // contentType: false,
-      success: function(data) {
-        alert(data);
+      data: {
+        sampleContent: file,
+        modelId: 'GeneralImageClassifier'
       },
+      processData: false,
+      contentType: false,
       headers: {
         Authorization:
-          'Bearer HVBKD5M6YCW74BSJTUDHJTUXQBTM34HVUXCHJBBJ3WUIXXW4HVJQIZOVX7FQZ5GPYEZYPMUCY7EJHLW43F2AXB2UJ2KSQOA6DSUDSFQ',
-        'Cache-Control': 'no-cache',
+          'Bearer 2ADD2POY2IYHK7JJ3HVA4PF5VOHF3D3S2TG7AH5TJO6UNI476WD762OTWVJYU4QYUB24ZS6LGHFELJRCFMT7KIG2PPPQBNDI646UETQ',
+        'Cache-Control': 'no-cache'
         'Content-Type': 'multipart/form-data'
       }
+    }).done(function(res) {
+      // format res in card format to show image and data
+      console.log(res);
     });
-    // .done(function(res) {
-    //   debugger;
-    //   // format res in card format to show image and data
-    //   console.log(res);
-    // });
   });
 });
