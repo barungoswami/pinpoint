4ebc308ce8704048992f770496b2f90d64d49461
= <=>
2017-10-19 17:50:44 -0700

    add portion of readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# PinPoint
+
+## REI Hiking Application
+
+## Responsive web/mobile application
+
+### PinPoint is an Image Recognition Application that allows users to take a photo and learn the details of the photo contents. Meant to be used while hiking, this Application aims to bridge the gap between objects in nature that hiker's know about, and those they do not know about. It's as simple as taking a photo of the unknown.
+
+##### If you get this error: No ‘Access-Control-Allow-Origin’ header is present on the requested resource. Origin ‘null’ is therefore not allowed access. You can use this Chrome plugin which allows all access.
+##### https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en
--- assets/js/camera.js
+++ assets/js/camera.js
@@ -7,34 +7,33 @@ $(document).ready(function() {
 
   uploadBtn.change(function(event) {
     event.preventDefault();
+    debugger;
     var file = event.target.files[0];
+    // console.log(file);
+    //
+    // var fd = new FormData();
+    // fd.append('file', input.files[0]);
 
     $.ajax({
       url: 'https://api.einstein.ai/v2/vision/predict',
       method: 'POST',
-      data: {
-        sampleContent: `@${file}`,
-        modelId: 'GeneralImageClassifier'
+      // data:
+      // processData: false,
+      // contentType: false,
+      success: function(data) {
+        alert(data);
       },
-      cache: false,
-      contentType: 'multipart/form-data',
-      crossDomain: true,
       headers: {
         Authorization:
-          'HVBKD5M6YCW74BSJTUDHJTUXQBTM34HVUXCHJBBJ3WUIXXW4HVJQIZOVX7FQZ5GPYEZYPMUCY7EJHLW43F2AXB2UJ2KSQOA6DSUDSFQ',
+          'Bearer HVBKD5M6YCW74BSJTUDHJTUXQBTM34HVUXCHJBBJ3WUIXXW4HVJQIZOVX7FQZ5GPYEZYPMUCY7EJHLW43F2AXB2UJ2KSQOA6DSUDSFQ',
         'Cache-Control': 'no-cache',
-        'Content-Type': 'multipart/form-data',
-        'Access-Control-Allow-Origin':
-          'https://api.einstein.ai/v2/vision/predict',
-        'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type'
+        'Content-Type': 'multipart/form-data'
       }
-      // 'Access-Control-Allow-Methods': 'POST',
-      // 'Access-Control-Allow-Headers':
-      // 'Origin, X-Requested-With, Content-Type, Accept',
-    }).done(function(res) {
-      // format res in card format to show image and data
-      console.log(res);
     });
+    // .done(function(res) {
+    //   debugger;
+    //   // format res in card format to show image and data
+    //   console.log(res);
+    // });
   });
 });
