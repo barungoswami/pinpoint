cf3945abed2c4b398ab559016e0a0b8d0b1e1752
= <=>
2017-10-21 11:09:42 -0700

    load photo card on done

--- assets/js/camera.js
+++ assets/js/camera.js
@@ -1,6 +1,3 @@
-
-// You can't upload a filename from Javascript, the file has to be selected by the user from an <input type="file">
-
 $(document).ready(function() {
   uploadImage();
 });
@@ -10,7 +7,6 @@ var uploadImage = function() {
   uploadBtn.change(function(event) {
     var formData = new FormData();
     var file = event.target.files[0];
-    // debugger;
 
     if ( window.FileReader ) {
       reader = new FileReader();
@@ -20,26 +16,45 @@ var uploadImage = function() {
       formData.append('sampleContent', file);
       formData.append('modelId', 'GeneralImageClassifier');
     }
+    // requestApiServer
+        $.ajax({
+          url: 'https://api.einstein.ai/v2/vision/predict',
+          method: 'POST',
+          headers: {
+            'Authorization':
+              'Bearer X6VERA4KVSPIWMLJ7TXIYBSIIOZNL36E7MZQW7NFADXQ6ZBE6YLWHTGCOR6EPJP43MPC5GJW2CBOAP6IGINTC25NSVYEFFEHCTDUQZY',
+            'Cache-Control': 'no-cache'
+          },
+          data: formData,
+          processData: false,
+          contentType: false
+        }).done(function(res) {
+          var hideContent = function(){
+              var descriptionText = $('.description');
+              var htmlMajor = $('.major');
+              uploadBtn.hide();
+              descriptionText.hide();
+              htmlMajor.text('Your Photo Results');
+            };
 
-// requestApiServer
-    $.ajax({
-      url: 'https://api.einstein.ai/v2/vision/predict',
-      method: 'POST',
-      headers: {
-        'Authorization':
-          'Bearer X6VERA4KVSPIWMLJ7TXIYBSIIOZNL36E7MZQW7NFADXQ6ZBE6YLWHTGCOR6EPJP43MPC5GJW2CBOAP6IGINTC25NSVYEFFEHCTDUQZY',
-        'Cache-Control': 'no-cache'
-        // 'Content-Type': 'multipart/form-data'
-      },
-      data: formData,
-      // formData: formData,
-      // cache: false,
-      processData: false,
-      contentType: false
-    }).done(function(res) {
-      // format res in card format to show image and data
-      console.log(res);
-      var probabilities = res.probabilities;
+            var showPhotoCard = function(){
+              var probabilities = res.probabilities[0];
+              var introArticle = $('#intro');
+              var htmlCard = introArticle.append($(`<div class="card"></div>`));
+              var htmlContainer = htmlCard.append($(`<div class="container"></div>`));
+              var label = probabilities.label.substring(0, probabilities.label.indexOf(","));
+              htmlContainer.append($(`<h4 class="label">PinPoint predicted that this is a ${label}.</h4>`))
+              // var htmlImage = htmlCard.append($(`<img src="${file.name}")}" style="width:100%"/>`))
+              var htmlNewImageButton = htmlCard.append($(`<input action="action" type="button" value="Take Another Photo" return false;" />`));
+              htmlNewImageButton.click(function(){
+                location.reload();
+              })
+
+              // $('.div_imagetranscrits').html('<img src="data:image/png;base64,' + data + '" />');
+            };
+
+            showPhotoCard();
+            hideContent();
+        });
     });
-  });
 };
